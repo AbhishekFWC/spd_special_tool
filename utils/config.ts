92e772321be1dbe0b9929f13557a6a14741d6263
Abhishek <imagine@MacBooks-MacBook-Air.local>
2023-08-15 18:05:00 +0530

    base url changed to production

--- utils/config.ts
+++ utils/config.ts
@@ -1,4 +1,5 @@
 // export const BASE_URL = 'http://localhost:5003/api/v1'
 
 
-export const BASE_URL = 'http://192.168.1.43:5003/api/v1';
+// export const BASE_URL = 'http://192.168.1.43:5003/api/v1';
+export const BASE_URL = "https://devgram.in/api/v1"
